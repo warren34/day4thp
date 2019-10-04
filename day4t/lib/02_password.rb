4dbae896a7c7c9e92eafdc5ecadf01b6407bbf9b
warren34 <55764682+warren34@users.noreply.github.com>
2019-10-04 05:51:44 +0200

    Update 02_password.rb

--- day4t/lib/02_password.rb
+++ day4t/lib/02_password.rb
@@ -8,14 +8,14 @@ end
 def login(psw)
     puts "confirmer votre mot de passe"
     psw_u = gets.chomp.to_i
-    while psw_u != mdp
+    while psw_u != psw
         puts "mot de passe erronee veuillez reesayer"
         psw_u = gets.chomp.to_i
     end
     return psw_u
 end
 
-def welcome_screen(psw_u,mdp)
+def welcome_screen(psw_u,psw)
     if psw_u == psw
         puts "you are welcome futur corsair now you are in the link"    
     end
